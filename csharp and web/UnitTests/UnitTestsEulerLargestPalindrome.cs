b13047c7ed0bab7e9daf62f43c2ad9cedc45fdb0
Derrick Bentley <bentleyd@mail.gvsu.edu>
2017-02-11 21:28:01 -0500

    finished question 4 with tests

---
+++ csharp and web/UnitTests/UnitTestsEulerLargestPalindrome.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using NUnit.Framework;
+using ProblemsClassLibrary;
+
+namespace UnitTests
+{
+    /// <summary>
+    /// Summary description for UnitTestsEulerProblem4
+    /// </summary>
+    public class UnitTestsEulerLargestPalindrome
+    {
+        private eulerLargestPalindrome elp;
+        public UnitTestsEulerLargestPalindrome()
+        {
+            elp = new eulerLargestPalindrome();
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [Test]
+        [TestCase("racecar", "racecar")]
+        [TestCase("abcdefg", "gfedcba")]
+        [TestCase("", "")]
+        [TestCase("a", "a")]
+        [TestCase("abcdefggfedcba", "abcdefggfedcba")]
+        [TestCase("Derrick", "kcirreD")]
+        public void Test_Is_Reverse_Word(String word, String expected)
+        {
+            Assert.AreEqual(elp.reversedWord(word), expected);
+        }
+
+        [Test]
+        [TestCase("racecar", true)]
+        [TestCase("abcdefg", false)]
+        [TestCase("", false)]
+        [TestCase("a", true)]
+        [TestCase("abcdefggfedcba", true)]
+        [TestCase("Derrick", false)]
+        public void Test_Is_Valid_Palindrome(String word, bool expected)
+        {
+            Assert.AreEqual(elp.isPalindrome(word), expected);
+        }
+
+        [Test]
+        [TestCase(0, 99, 9009)]
+        [TestCase(100, 999, 906609)]
+        public void Test_Answer_To_Question_4(int from, int upto, int expected)
+        {
+            Assert.AreEqual(elp.findLargestPalindrome(from, upto), expected);
+        }
+            
+    }
+}
--- csharp and web/UnitTests/UnitTestsEulerSmallestMultiple.cs
+++ csharp and web/UnitTests/UnitTestsEulerSmallestMultiple.cs
@@ -9,18 +9,17 @@ namespace UnitTests
     /// <summary>
     /// Summary description for UnitTest1
     /// </summary>
-    public class UnitTestsEuler
+    public class UnitTestsEulerSmallestMultiple
     {
 
         private eulerSmallestMultiple eulerSmallestMultiple;
-        public UnitTestsEuler()
+
+        public UnitTestsEulerSmallestMultiple()
         {
-            //
-            // TODO: Add constructor logic here
-            //
             eulerSmallestMultiple = new eulerSmallestMultiple();
         }
 
+        #region still need to find what this is
         private TestContext testContextInstance;
 
         /// <summary>
@@ -38,6 +37,7 @@ namespace UnitTests
                 testContextInstance = value;
             }
         }
+        #endregion
 
         #region Additional test attributes
         //
@@ -64,7 +64,7 @@ namespace UnitTests
         [Test]
         [TestCase(1, 10, 2520, true)]
         //[TestCase(1, 20, 232792560, true)]
-        public void euler_problem_5_isDivisibleEvenly(int from, int upto, int number, bool expected)
+        public void Test_Euler_problem_5_isDivisibleEvenly(int from, int upto, int number, bool expected)
         {
             Assert.AreEqual(eulerSmallestMultiple.isDivisibleEvenly(from, upto, number), expected);
         }
@@ -72,7 +72,7 @@ namespace UnitTests
         [Test]
         [TestCase(1, 10,2520)]
         //[TestCase(1, 20, 232792560)]
-        public void euler_problem_5_findSmallestMultiple(int from, int upto, int expected)
+        public void Test_Euler_problem_5_findSmallestMultiple(int from, int upto, int expected)
         {
             Assert.AreEqual(eulerSmallestMultiple.findSmallestMultipleWithRange(from, upto), expected);
         }
