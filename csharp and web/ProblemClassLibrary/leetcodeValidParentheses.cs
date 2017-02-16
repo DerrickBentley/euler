26d63c74dd92be15e9cced515e3c1b81d864592f
Derrick Bentley <bentleyd@mail.gvsu.edu>
2017-02-15 21:31:59 -0500

    adding valid Parentheses problem

---
+++ csharp and web/ProblemClassLibrary/leetcodeValidParentheses.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemClassLibrary
+{
+    /// <summary>
+    /// Given a string containing just the characters '(', ')', '{', '}', '[' and ']',
+    /// determine if the input string is valid.
+    /// The brackets must close in the correct order, "()" and "()[]{}" are all valid 
+    /// but "(]" and "([)]" are not.
+    /// </summary>
+    public class leetcodeValidParentheses
+    {
+        private static readonly char[] opens = { '(', '[', '{' };
+        private static readonly char[] closes = { ')', ']', '}' };
+
+        public Boolean isValid(String s)
+        {
+            Stack<char> stack = new Stack<char>();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (opens.Contains(s[i]))
+                {
+                    stack.Push(s[i]);
+                }
+                else
+                {
+                    int closeType = Array.IndexOf(closes, s[i]);
+                    if (closeType < 0)
+                        return false;
+                    if (stack.Peek() == opens[closeType])
+                    {
+                        stack.Pop();
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+            return stack.Count == 0;
+        }
+    }
+}
---
+++ csharp and web/UnitTests/UnitTestsLeetcodeValidParentheses.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using NUnit.Framework;
+using NUnit.Framework.Constraints;
+using ProblemClassLibrary;
+
+namespace UnitTests
+{
+    /// <summary>
+    /// Unit Tests for  LeetcodeValidParentheses
+    /// </summary>
+    public class UnitTestsLeetcodeValidParentheses
+    {
+        private leetcodeValidParentheses lcVP;
+        public UnitTestsLeetcodeValidParentheses()
+        {
+            lcVP = new leetcodeValidParentheses();
+        }
+
+        [Test]
+        [TestCase("[](){}", true)]
+        [TestCase("([{}])", true)]
+        [TestCase("([{(()[]{})[(){}[]]{{}()[]}}])", true)]
+        [TestCase("{]", false)]
+        [TestCase("{([]})", false)]
+        [TestCase("{{}}[[]](()]", false)]
+        public void Test_Is_Valid_Parentheses(String s, Boolean expected)
+        {
+            Assert.AreEqual(lcVP.isValid(s), expected);
+        }
+    }
+}
